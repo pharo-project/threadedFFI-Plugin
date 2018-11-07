530352366874ed76b012bdc9b04b5ff7cf4d6abc
Guille Polito <guillermopolito@gmail.com>
2018-11-07 01:20:43 +0100

    Refactored callout test functions

--- tests/src/c/callout.c
+++ tests/src/c/callout.c
@@ -1,21 +1,40 @@
 #include "testLibrary.h"
 #include <unistd.h>
 
+//Deprecated
 int returnAnswer(){
 	return 42;
 }
 
+//Deprecated
 int sumTwoNumbers(int a, int b){
   return a+b; 
 }
 
-
+//Deprecated
 float sumAFloatAndADouble(float a, double b){
   return a+b; 
 }
 
+int shortCallout(){
+  return 42;
+}
 
-int withDelay(int seconds){
+int longCallout(int seconds){
 	usleep(seconds * 1000000); // will sleep for number of seconds
   return 1;
+}
+
+int multipleArgumentCallout(
+  int arg1,
+  int arg2,
+  int arg3,
+  int arg4,
+  int arg5,
+  int arg6,
+  int arg7,
+  int arg8,
+  int arg9,
+  int arg10){
+  return arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10;
 }
