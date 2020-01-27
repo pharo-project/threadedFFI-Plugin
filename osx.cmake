ceb008cfcbb729bf1f81a65af0286b537ca0501a
Pablo Tesone <tesonep@gmail.com>
2020-01-27 15:11:39 +0100

    Improving the execution in a different build directory

--- osx.cmake
+++ osx.cmake
@@ -1,10 +1,11 @@
 set(HEADER_URL "https://ci.inria.fr/pharo-ci-jenkins2/job/pharo-vm/job/headless/94/artifact/build/build/packages/PharoVM-8.1.0-02626ec-mac64-include.zip") 
 set(LIBFFI_TARGET "x86_64-apple-darwin")
 
-set(LIBFFI_OUTPUT "${CMAKE_SOURCE_DIR}/build/bin/libffi.dylib" "${CMAKE_SOURCE_DIR}/build/bin/libffi.7.dylib")
+set(LIBFFI_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/build/bin/libffi.dylib" "${CMAKE_CURRENT_BINARY_DIR}/build/bin/libffi.7.dylib")
 set(PHARO_PLUGIN_DIR pharo-vm/Pharo.app/Contents/MacOS/Plugins/)
 
 set(TEST_LIBRARY_DLL libTestLibrary.dylib) 
 
 set(DYLIB_EXT "*.dylib")
 
+set(CMAKE_CURRENT_SOURCE_DIR_TO_OUT ${CMAKE_CURRENT_SOURCE_DIR})
--- win.cmake
+++ win.cmake
@@ -1,7 +1,7 @@
 set(HEADER_URL "https://ci.inria.fr/pharo-ci-jenkins2/job/pharo-vm/job/headless/94/artifact/build/build/packages/PharoVM-8.1.0-02626ece-win64-include.zip") 
 set(LIBFFI_TARGET "x86_64-unknown-cygwin")
 
-set(LIBFFI_OUTPUT "${CMAKE_SOURCE_DIR}/build/libffi/install/bin/ffi-7.dll")
+set(LIBFFI_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/build/libffi/install/bin/ffi-7.dll")
 set(PHARO_PLUGIN_DIR pharo-vm/)
 
 set(TEST_LIBRARY_DLL TestLibrary.dll) 
@@ -14,4 +14,11 @@ set(CMAKE_SHARED_MODULE_PREFIX "")
 set(CMAKE_SHARED_MODULE_SUFFIX ".dll")    
 
 
-set(LIBFFI_FILES "${CMAKE_SOURCE_DIR}/build/libffi/install/bin/cygffi-7.dll")
+set(LIBFFI_FILES "${CMAKE_CURRENT_BINARY_DIR}/build/libffi/install/bin/cygffi-7.dll")
+
+# transform the path into a windows path with unix backslashes C:/bla/blu
+# this is the path required to send as argument to libraries outside of the control of cygwin (like pharo itself)
+execute_process(
+	COMMAND cygpath ${CMAKE_CURRENT_SOURCE_DIR} --mixed
+	OUTPUT_VARIABLE CMAKE_CURRENT_SOURCE_DIR_TO_OUT
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
