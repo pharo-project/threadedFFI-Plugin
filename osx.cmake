67974b5739cf376b22fb5c14fab3c10258545ee4
Pablo Tesone <tesonep@gmail.com>
2020-01-31 14:54:04 +0100

    Changing the location of the library

--- osx.cmake
+++ osx.cmake
@@ -1,4 +1,4 @@
-set(HEADER_URL "https://ci.inria.fr/pharo-ci-jenkins2/job/pharo-vm/view/change-requests/job/PR-54/2/artifact/build/build/packages/PharoVM-8.2.0-cfe4e2c-mac64-include.zip") 
+set(HEADER_URL "https://ci.inria.fr/pharo-ci-jenkins2/job/pharo-vm/job/idle/9/artifact/build/build/packages/PharoVM-8.3.0-ed72d63-mac64-include.zip") 
 set(LIBFFI_TARGET "x86_64-apple-darwin")
 
 set(LIBFFI_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/build/bin/libffi.dylib" "${CMAKE_CURRENT_BINARY_DIR}/build/bin/libffi.7.dylib")
